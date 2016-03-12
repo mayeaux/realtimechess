00e7339e1faebea5ddb6e8fe322b75d5b71811cf
Anthony Mayfield <instacoins@gmail.com>
2016-03-11 21:01:42 -0600

    changing port for heroku

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 
 app.configure(function() {
   console.log(process.env.PORT)
+  console.log(process.env)
   app.set('ipaddress', process.env.HEROKU_IP || '127.0.0.1');
   app.set('port', process.env.HEROKU_PORT || 3000);
   app.set('views', __dirname + '/views');
