77ee457a90f0d76fd7e59c16a449dd6210c47580
Anthony <contact@anthonymayfield.com>
2016-06-03 19:01:49 -0700

    trying something new

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,13 +2,32 @@ var $URL, $socket;
 
 $(function () {
 
+  // var ENV = 'dev';
+  // var $WS;
+  //
+  // var data = 5000;
+  //
+  // if (ENV === 'dev') {
+  //   // $URL = 'https://real-time-chess.herokuapp.com:' + data;
+  //   $URL = 'http://localhost:' + data;
+  //   $WS = $URL;
+  // } else if (ENV === 'heroku') {
+  //   $URL = 'https://real-time-chess.herokuapp.com/';
+  //   $WS = 'https://real-time-chess.herokuapp.com:8000/';
+  // }
+  //
+  // $socket = io.connect($WS);
+
+  //
   $.get("/port", function( data ) {
+    console.log(data);
     //var ENV = 'openshift';
     var ENV = 'dev';
     var $WS;
 
     if (ENV === 'dev') {
-      $URL = 'https://real-time-chess.herokuapp.com/' + data;
+      $URL = 'https://real-time-chess.herokuapp.com:' + data;
+      // $URL = 'https://localhost:' + data;
       $WS = $URL;
     } else if (ENV === 'heroku') {
       $URL = 'https://real-time-chess.herokuapp.com/';
