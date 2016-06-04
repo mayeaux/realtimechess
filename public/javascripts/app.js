e8bb0d099356249ccc211e6e9bf746ce460cd785
Anthony <contact@anthonymayfield.com>
2016-06-03 18:17:18 -0700

    trying something new

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,16 @@
 var $URL, $socket;
-  
+
 $(function () {
   //var ENV = 'openshift';
-  var ENV = 'dev';
+  var ENV = 'heroku';
   var $WS;
 
   if (ENV === 'dev') {
     $URL = 'http://127.0.0.1:3000';
     $WS = $URL;
   } else if (ENV === 'heroku') {
-    $URL = 'http://real-time-chess.herokuapp.com/';
-    $WS = 'http://real-time-chess.herokuapp.com:8000/';
+    $URL = 'https://real-time-chess.herokuapp.com/';
+    $WS = 'https://real-time-chess.herokuapp.com:8000/';
   }
 
   $socket = io.connect($WS);
